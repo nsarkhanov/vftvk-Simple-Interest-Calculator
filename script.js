60dbe6c4ee408d4567ade002298d6426dfa80b65
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-07-09 10:37:44 +0200

    finished

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function e(p,y,r){
+function checkerror(p,y,r){
 
     if (p==null || p==""){
         alert("Amount must be filled out");
@@ -16,6 +16,25 @@ function e(p,y,r){
             return true;
         }
 }
+function checkdata(p,r,y){
+     if (p<=0){
+        alert("Amount must be bigger than  0 ");
+        return false;
+        }
+          if (r<=0) {
+        alert("Rate must be bigger than  0 ");
+        return false;
+        }
+      if (y<=0){
+        alert(" Year  must be bigger than  0 ");
+        return false;
+        }
+        else{
+            return true;
+        }
+
+
+}
 function compute()
 {
     var p = document.getElementById("principal").value;
@@ -26,14 +45,16 @@ function compute()
     result = parseInt(p*r*y) ;
     // document.getElementById("result").innerHTML ="If you deposit " +p+"at an interest rate " +r+" You will receive an amount of "+result+", in the "+year;
     // document.getElementById("deposit").innerHTML = "write me to the screen";
-    var check=e(p,y,r)
-    if (check==true){
+    var checkerr=checkerror(p,y,r)
+    var checkd=checkdata(p,r,y)
+    if( checkerr==true && checkd==true){
 
     document.getElementById("result").innerHTML ="If you deposit " +p+"at an interest rate " +r+" You will receive an amount of "+result+", in the "+year;
     // document.getElementById("deposit").innerHTML = "write me to the screen";
     }
     else {
-        e(p,y,r)
+        checkerror(p,y,r)
+      
     }
    
         
